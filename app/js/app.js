0628bcdcc364da6293f7b209d44d6cbb9ad92cce
Neeraj Joshi <neeraj.joshi@3pillarglobal.com>
2014-01-14 16:14:06 +0530

    Adding a thank you tab

--- app/js/app.js
+++ app/js/app.js
@@ -14,5 +14,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/movies', {templateUrl: 'partials/movies.html', controller: 'MoviesCtrl'});
   $routeProvider.when('/movie/delete/:movieId/', {templateUrl: 'partials/movies.html', controller: 'MovieDeleteCtrl'});
+  $routeProvider.when('/thankyou', {templateUrl: 'partials/thankyou.html'});
   $routeProvider.otherwise({redirectTo: '/movies'});
 }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -9,19 +9,33 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }])
-  .directive( 'fbimage', function() {
+  .directive( 'wish', function() {
+	  return {
+		     restrict: 'E',
+		     link:function(scope, element, attrs) {
+		       var greet = attrs.greet;
+		       var tag = '';
+		       if ((greet !== null) && (greet !== undefined) && (greet !== '')) {
+		    	   tag = '<embed src="http://apps.afterbtech.com/swf/'+greet+'.swf" width="550" height="400" wmode="transparent" />';
+		       }
+		       element.append(tag);
+		     }
+		   }
+  })
+    .directive( 'fbimage', function() {
 	  return {
 		     restrict: 'E',
 		     link:function(scope, element, attrs) {
 		       var fbid = attrs.fbid;
 		       var tag = '';
 		       if ((fbid !== null) && (fbid !== undefined) && (fbid !== '')) {
-		    	      tag = '<img src="http://graph.facebook.com/' + fbid + '/picture?type=large" class="img-responsive"/>'
+		    	   tag = '<img src="http://graph.facebook.com/' + fbid + '/picture?type=large" class="img-responsive"/>'
 		       }
 		       element.append(tag);
 		     }
 		   }
   })
+  //http://apps.afterbtech.com/swf/thank_you.swf
   .directive( 'editInPlace', function() {
   return {
     restrict: 'E',
